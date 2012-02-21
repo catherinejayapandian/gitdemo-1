053d714373cabd5cb92b38259048f207ce272a73
Remo Mueller <remosm@gmail.com>
2012-02-21 16:51:08 -0500

    initial rails 3.2.1 files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitdemo::Application.config.secret_token = '33f61d66d227e4432f46545656284b59fee0a4e3d35e4241f526774647d3ada78e30180aab85015821f874ed9004f78d0c2ddfcbafb16d4ba958d5144ac7ea41'
